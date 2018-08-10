c665714bf13a42366332c7d5e3d5f0423e92cb99
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-08-10 15:15:28 +0200

    feat: show user status messages

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 io.on('connection', (socket) => {
+
+  // Broadcast when a user connects
   console.log('a user connected')
-  io.emit('chat message', 'a user connected')
-  socket.on('chat message', (msg) => io.emit('chat message', msg))
+  socket.broadcast.emit('user status', { type: 'connect', msg: 'a user connected' })
+
+  // Broadcast when a user disconnects
   socket.on('disconnect', () => {
     console.log('a user disconnected')
-    io.emit('chat message', 'a user disconnected')
+    socket.broadcast.emit('user status', { type: 'disconnect', msg: 'a user disconnected' })
   })
+
+  // Show messages from users
+  socket.on('chat message', (msg) => io.emit('chat message', msg))
 })
 
-http.listen(3000, () => console.log('listening on *:3000'))
+http.listen(port, () => console.log(`listening on *:${port}`))
