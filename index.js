36a1f863209cd81123ee789d073a41a144243f0b
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-08-13 15:15:34 +0200

    feat: assign user a random username

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
+const connectedUserMap = new Map()
+
+const randomWords = require('random-words')
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 io.on('connection', (socket) => {
+
+  // Set username
+  const connectedUserId = socket.id;
+  connectedUserMap.set(socket.id, { status:'online', name: randomWords()});
 
   // Broadcast when a user connects
   console.log('a user connected')
@@ -18,7 +25,10 @@ io.on('connection', (socket) => {
   })
 
   // Show messages from users
-  socket.on('chat message', (msg) => io.emit('chat message', msg))
+  socket.on('chat message', (msg) => {
+    const user = connectedUserMap.get(connectedUserId)
+    io.emit('chat message', { msg: msg, name: user.name })
+  })
 })
 
 http.listen(port, () => console.log(`listening on *:${port}`))
