5790013f9c81af093ec1dcbb3e840363679b0dc4
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-08 16:23:34 +0700

    feat: create a socket.io connection then save username

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = process.env.PORT || 3001
-const app = express()
 
-app.use(express.static('public'))
+const port = process.env.PORT || 3001
+const users = {}
+
 app.use(cors())
 
 app.post('/post', bodyParser.json(), (req, res) => {
@@ -12,6 +15,21 @@ app.post('/post', bodyParser.json(), (req, res) => {
   res.end()
 })
 
-app.listen(port, () => {
+// Socket.io client connection
+io.on('connection', (client) => {
+
+  // client join event handler
+  client.on('join', (user) => {
+    // adds new user with its id as index
+    users[user.id] = {
+      name: user.name
+    }
+    console.log('User %s connected', user.name)
+  })
+
+
+})
+
+server.listen(port, () => {
   console.log('Server listening on port %s, Ctrl+C to stop', port)
 })
