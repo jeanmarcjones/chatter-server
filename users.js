a7a758726954244319855bc5a017c4c7efe2f7b3
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-01-05 14:45:21 +0700

    feat: user curd functions

--- users.js
+++ users.js
@@ -1,1 +1,41 @@
 const users = {}
+
+function getKeys() {
+  return Object.keys(users)
+}
+
+function getByKey(key) {
+  return users[key]
+}
+
+function getBySession(sId) {
+  let key = getKeys().reduce((key) => {
+    if (users[key].id === sId)
+      return users[key]
+  })
+  return {
+    ...users[key],
+    key: key
+  }
+}
+
+function getAll() {
+  return users
+}
+
+function add(key, user) {
+  users[key] = {
+    ...user
+  }
+}
+
+function remove(id) {
+  delete users[id]
+}
+
+function update(key, user) {
+  users[key] = {
+    ...users[key],
+    ...user
+  }
+}
